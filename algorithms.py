14350bd227a7d621125cab2a036a2ba76985f873
Asmahan <asmahan.alexander@gmail.com>
2025-10-06 19:51:00 +0300

    Integrating functions into an interactive user menu system

--- algorithms.py
+++ algorithms.py
@@ -42,3 +42,56 @@ def login():
         print("Access denied. Please try again.\n")
         return False
 
+
+# MAIN PROGRAM 
+logged_in = False
+
+while True:
+    print("\nChoose an option:")
+    print("1. Factorial")
+    print("2. Find Max")
+    print("3. Linear Search")
+    print("4. Fibonacci")
+    print("5. Login")
+    print("6. Exit")
+
+    choice = input("Enter your option: ")
+
+    if choice == "1":
+        if not logged_in:
+            print("Please login first.")
+            continue
+        n = int(input("Enter a number: "))
+        print("Factorial:", factorial(n))
+
+    elif choice == "2":
+        if not logged_in:
+            print("Please login first.")
+            continue
+        nums = list(map(int, input("Enter numbers separated by space: ").split()))
+        print("Max number:", find_max(nums))
+
+    elif choice == "3":
+        if not logged_in:
+            print("Please login first.")
+            continue
+        nums = list(map(int, input("Enter numbers separated by space: ").split()))
+        target = int(input("Enter number to search: "))
+        print("Found:", linear_search(nums, target))
+
+    elif choice == "4":
+        if not logged_in:
+            print("Please login first.")
+            continue
+        n = int(input("Enter n for Fibonacci: "))
+        print("Fibonacci number:", fibonacci_iterative(n))
+
+    elif choice == "5":
+        logged_in = login()
+
+    elif choice == "6":
+        print("Exiting program...")
+        break
+
+    else:
+        print("Invalid option. Try again.")
